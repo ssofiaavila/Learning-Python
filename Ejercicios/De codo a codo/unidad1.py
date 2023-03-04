91c5600a43651dd00f321734f9444510b015ad3a
soso <avilasofia076@gmail.com>
2023-03-04 17:38:03 -0300

    Update unidad1.py

--- Ejercicios/De codo a codo/unidad1.py
+++ Ejercicios/De codo a codo/unidad1.py
@@ -1,2 +1,10 @@
 # 1) escribe un programa que muestre en pantalal "Hello World" #
 print("Hello World")
+
+# 2) Escriba un programa que escriba el resultado de sumar 3+5#
+print(3+5)
+
+''' 3) Escribe un programa que pida el nombre del usuario y escriba un texto que diga
+"Hola nombreUsuario" '''
+nombreUsuario= input("Ingrese nombre de usuario ")
+print("Hola " +nombreUsuario)
