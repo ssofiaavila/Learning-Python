0fafa936727bb3795a67db21d7da23120e62983c
soso <avilasofia076@gmail.com>
2023-03-06 12:39:57 -0300

    Create unidad2.py

---
+++ Ejercicios/unidad2.py
@@ -0,0 +1,5 @@
+# 1) Desarrollar una función que reciba tres números positivos y devuelva el mayor de los 
+# tres, sólo si éste es único(mayor estricto). En caso de no existir el mayor estricto 
+# devolver - 1. No utilizar operadores lógicos ( and , or , not). Desarrollar también un
+# programa para ingresar los tres valores, invocar a la función y mostrar el máximo 
+# hallado, o un mensaje informativo si éste no existe.
