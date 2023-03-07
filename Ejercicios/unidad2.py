73fd7b0ecc2bb59ab9245725e86d70ba5ab218ba
soso <avilasofia076@gmail.com>
2023-03-07 19:24:22 -0300

    Update unidad2.py

--- Ejercicios/unidad2.py
+++ Ejercicios/unidad2.py
@@ -3,3 +3,15 @@
 # devolver - 1. No utilizar operadores lógicos ( and , or , not). Desarrollar también un
 # programa para ingresar los tres valores, invocar a la función y mostrar el máximo 
 # hallado, o un mensaje informativo si éste no existe.
+
+#función
+def hayarMayor(num1,num2,num3):
+
+    return max
+
+# programa principal
+num1= int(input("Ingrese el primer número"))
+num2= int(input("Ingrese el segundo número"))
+num3= int(input("Ingrese el tercer número"))
+maximo= hayarMayor(num1,num2,num3)
+print("El numero mayor es {}").format(maximo)
